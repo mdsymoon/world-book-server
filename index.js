576eb9cd5b1966b824698a6063dae931de8f5bb5
mdsymoon <mdsymoon11@gmail.com>
2021-11-18 16:29:13 -0800

    crated post request for book list

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient } = require("mongodb");
+const app = express();
+const fileUpload = require("express-fileupload");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT || 4000;
+
+// middleWare
+
+app.use(cors());
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o8cqw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const BookCollection = client.db("WorldBook").collection("BookList");
+
+  app.post("/addBook", (req, res) => {
+    const { name, price, category } = req.body;
+    const file = req.files.file;
+
+    const newImg = file.data;
+    const encImg = newImg.toString("base64");
+
+    const img = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, "base64"),
+    };
+    BookCollection
+      .insertOne({ name, price, category, img })
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+  });
+
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
